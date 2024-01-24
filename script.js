529ecffc186b76a322ce735cd481a2f92998a7c4
ErLions94 <114865892+ErLions94@users.noreply.github.com>
2024-01-25 00:41:19 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var smallScreenText = document.querySelector('.small-screen-text');
-var largeScreenText = document.querySelector('.large-screen-text');
-
-function toggleText() {
-    if (smallScreenText.style.display === 'block') {
-        smallScreenText.style.display = 'none';
-        largeScreenText.style.display = 'block';
-    }
-}
